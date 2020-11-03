29f217e4dc1597fb543c1af7f02001388165a72d
deepthi <deepsy.weasly@gmail.com>
2020-11-03 15:00:24 +0100

    added 2 lines in wsgi file, 1 import and another assignemnt

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+import django.core.handlers.wsgi 
+aplication=django.core.handlers.wsgi.WSGIHandler()
